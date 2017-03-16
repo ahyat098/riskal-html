15f4fd08ebc6d1264cf0df7a699bc3d162d7a172
ahyat098 <y4y4t07@gmail.com>
2017-03-16 12:06:54 +0700

    Update share.js

--- share.js
+++ share.js
@@ -2,6 +2,6 @@
   (function() {
     var dr = document.createElement('script');
     dr.type = 'text/javascript'; dr.async = true;
-    dr.src = '//cdn.rawgit.com/bachors/jQuery-Awesome-Sosmed-Share-Button/2b6fc79a/src/js/ayoshare.js';
+    dr.src = '//cdn.rawgit.com/bachors/jQuery-Awesome-Sosmed-Share-Button/2b6fc79a/src/js/ayoshare.min.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
   })();
